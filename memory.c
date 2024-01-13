e46f979fee3f26fdece234c7bef00ec1391a671e
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2024-01-13 02:57:42 +0100

    Create memory.c

---
+++ memory.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+
+/**
+ * bfree - this frees a pointer and NULLs the address
+ * @ptr: the address of the pointer to free
+ * Return: returns 1 if freed, otherwise 0.
+ */
+int bfree(void **ptr)
+{
+	if (ptr && *ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
+	}
+	return (0);
+}
